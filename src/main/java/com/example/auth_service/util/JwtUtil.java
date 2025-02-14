78777c79c6118b07c3a3dfd6ae532890d42723c2
Michael McGowan <mcgoo.uk@gmail.com>
2025-02-14 21:31:12 +0000

    debug

--- src/main/java/com/example/auth_service/util/JwtUtil.java
+++ src/main/java/com/example/auth_service/util/JwtUtil.java
@@ -1,12 +1,13 @@
 package com.example.auth_service.util;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
-
 import java.security.Key;
 import java.util.Date;
 import java.util.function.Function;
